c95c92a21b323e334afbd0f85fe357b9e36970fa
Cynthia Sun <cynthia0828@gmail.com>
2014-11-09 19:22:54 -0500

    Update Plot 2.R

--- Project-1/Plot 2.R
+++ Project-1/Plot 2.R
@@ -1,7 +1,7 @@
 
 rm(list=ls())
 #reading data into R:
-cons<- read.table("household_power_consumption.txt", sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+> cons<-read.table('H:/Desktop/Data Specialist/Exploratory Data Analysis/Project1/exdata_data_household_power_consumption/household_power_consumption.txt',sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
 # Subsetting the full data to obtain the data related to two days:
 sub<- subset(cons, (cons$Date == "1/2/2007" | cons$Date== "2/2/2007"))
 # Changing the class of Date variable from character to Date:
